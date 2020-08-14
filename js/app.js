cbae4a700434d4844b834c7b3202b45dee2a8fe3
Vitor <vitorvieira.antunes@gmail.com>
2020-08-13 21:21:11 -0400

    firt commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const progFeaturedImg = document.getElementById("progFeaturedImg");
+progFeaturedImg.addEventListener("click", () => {
+    document.getElementById("modalImg").src = "assets/img/Diver_screenshot.jpg";
+
+});
+
+const progImg1 = document.getElementById("progImg1");
+progImg1.addEventListener("click", () => {
+    document.getElementById("modalImg").src = "assets/img/BrewApp.jpg";
+
+});
+const progImg2 = document.getElementById("progImg2");
+progImg2.addEventListener("click", () => {
+    document.getElementById("modalImg").src = "assets/img/stats_screenshot.jpg";
+
+});
